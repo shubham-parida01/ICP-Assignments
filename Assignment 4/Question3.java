cab746aa09a6c1cfcbd93e7ebf91e6e9a37a3ec8
Shubham Parida <127040084+shubham-parida01@users.noreply.github.com>
2025-01-07 00:03:13 +0530

    Update Question3.java

--- Assignment 4/Question3.java
+++ Assignment 4/Question3.java
@@ -5,15 +5,22 @@ public class Question3 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
-        int random ;
+        int count = n;
+        int random;
         int res = 0;
-        for ( int i = 0;i<n ; i++){
-            random = (int)(Math.random()*(n-1)) + 1;
-            System.out.print(random+" ");
+        int i = 0;
+
+        System.out.println("Generated random numbers:");
+
+        do {
+            random = (int) (Math.random() * 100) + 1;
+            System.out.print(random + " ");
             res += random;
-        }
-        res /= n;
+            i++;
+        } while (i < count);
+
+        double average = (double) res / count;
         System.out.println();
-        System.out.println("Average of "+n+ " random numbers are "+res);
+        System.out.println("Average of " + count + " random numbers is " + average);
     }
 }
